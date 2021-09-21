fcf6b341a1f37b0aa79fd8e58aa1e724e89325b5
Maksym Davydenko <maksydenko@gmail.com>
2021-09-21 19:00:11 +0300

    Practice 1

--- practice_01.py
+++ practice_01.py
@@ -1,10 +1,13 @@
 # First task
+
 print("Hello world!")
 
 # Second task
+
 print("How are you?\nThis is my first practice\nThat's wonderful!")
 
 # Third task
+
 lenght = float(input("Write the length of the rectangle: "))
 width = float(input("Write the width of the rectangle: "))
 
@@ -12,6 +15,7 @@ square = lenght * width
 print("Square: " + str(square))
 
 # Fourth task
+
 first = float(input("Write the first number: "))
 second = float(input("Write the second number: "))
 
@@ -19,9 +23,10 @@ plus = first + second
 multiply = first * second
 minus = first - second
 divide = first / second
-print("Sum: " + str(plus) + ", product: " + str(multiply) + ", difference: " + str(minus) + ", quotient: " + str(divide))
+print("Sum: " + str(plus) + "; product: " + str(multiply) + "; difference: " + str(minus) + "; quotient: " + str(divide))
 
 # Fifth task
+
 import math
 
 radius = float(input("Write the radius of the circle: "))
@@ -29,9 +34,10 @@ radius = float(input("Write the radius of the circle: "))
 diameter = radius * 2
 circumference = diameter * math.pi
 area = radius ** 2 * math.pi
-print("Diameter: " + str(diameter) + ", circumference: " + str(circumference) + ", area: " + str(area))
+print("Diameter: " + str(diameter) + "; circumference: " + str(circumference) + "; area: " + str(area))
 
 # Sixth task
+
 number = int(input("Write the three-digit number: "))
 
 if number > 999:
@@ -45,4 +51,5 @@ else:
     sum = one + two + three
     difference = one - two - three
     product = one * two * three
-    print("Sum: " + str(sum) + ", difference: " + str(difference) + ", product: " + str(product))
+    print("Sum: " + str(sum) + "; difference: " + str(difference) + "; product: " + str(product))
+    
