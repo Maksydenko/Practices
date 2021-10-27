9c721cef3f10748dd148049309d0436009424e74
Maksym Davydenko <maksydenko@gmail.com>
2021-10-27 12:39:34 +0300

    Laboratory work 3

--- laboratory_work_03.py
+++ laboratory_work_03.py
@@ -1,5 +1,5 @@
 int_1 = int(input("Write the first integer: "))
-print("Special string with '%': ", "%5d" % int_1, \
+print("Special string with \"%\": ", "%5d" % int_1, \
       "\nThe string format() method: ", "{0:5d}".format(int_1),
       "\nf-string: ", f"{int_1:5d}")
 int_2 = int(input("Write the second integer: "))
@@ -8,19 +8,19 @@ print("Special string with '%': ", "%5d" % int_2,
       "\nf-string: ", f"{int_2:5d}")
 
 float_1 = float(input("Write the first real number: "))
-print("Special string with '%': ", "%8f" % float_1,
+print("Special string with \"%\": ", "%8f" % float_1,
       "\nThe string format() method: ", "{0:8f}".format(float_1),
       "\nf-string: ", f"{float_1:8f}")
 float_2 = float(input("Write the second real number: "))
-print("Special string with '%': ", "%8f" % float_2,
+print("Special string with \"%\": ", "%8f" % float_2,
       "\nThe string format() method: ", "{0:8f}".format(float_2),
       "\nf-string: ", f"{float_2:8f}")
 float_3 = float(input("Write the third real number: "))
-print("Special string with '%': ", "%7.4f" % float_3,
+print("Special string with \"%\": ", "%7.4f" % float_3,
       "\nThe string format() method: ", "{0:7.4f}".format(float_3),
       "\nf-string: ", f"{float_3:7.4f}")
 float_4 = float(input("Write the fourth real number: "))
-print("Special string with '%': ", "%7.4f" % float_4,
+print("Special string with \"%\": ", "%7.4f" % float_4,
       "\nThe string format() method: ", "{0:7.4f}".format(float_4),
       "\nf-string: ", f"{float_4:7.4f}")
 
