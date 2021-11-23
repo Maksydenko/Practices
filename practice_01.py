844685d30bff8dbc123c482dee943999ec3901a3
Maksym Davydenko <maksydenko@gmail.com>
2021-11-23 14:35:58 +0200

    Practice 1

--- practice_01.py
+++ practice_01.py
@@ -1,55 +1,46 @@
-# First task
+import math
+
+# Task 1
 
 print("Hello world!")
 
-# Second task
+# Task 2
 
-print("How are you?\nThis is my first practice\nThat's wonderful!")
+print("How are you?\n\
+This is my first practice\n\
+That's wonderful!")
 
-# Third task
+# Task 3
 
 lenght = float(input("Write the length of the rectangle: "))
 width = float(input("Write the width of the rectangle: "))
+print(f"Square: {lenght * width}")
 
-square = lenght * width
-print("Square: " + str(square))
-
-# Fourth task
+# Task 4
 
 first = float(input("Write the first number: "))
 second = float(input("Write the second number: "))
+print(f"Sum: {first + second}; product: {first * second}; \
+difference: {first - second}; \quotient: {first / second}")
 
-plus = first + second
-multiply = first * second
-minus = first - second
-divide = first / second
-print("Sum: " + str(plus) + "; product: " + str(multiply) + "; difference: " + str(minus) + "; quotient: " + str(divide))
-
-# Fifth task
-
-import math
+# Task 5
 
 radius = float(input("Write the radius of the circle: "))
+print(f"Diameter: {radius * 2};circumference: {radius * 2 * math.pi}; \
+area: {radius ** 2 * math.pi}")
 
-diameter = radius * 2
-circumference = diameter * math.pi
-area = radius ** 2 * math.pi
-print("Diameter: " + str(diameter) + "; circumference: " + str(circumference) + "; area: " + str(area))
-
-# Sixth task
+# Task 6
 
 number = int(input("Write the three-digit number: "))
 
-if number > 999:
-    print("Your number isn't three-digit")
-elif number < 100:
-    print("Your number isn't three-digit")
-else:
+if 99 < number < 1000:
     one = number // 100
     two = number // 10 % 10
     three = number % 10
     sum = one + two + three
     difference = one - two - three
     product = one * two * three
-    print("Sum: " + str(sum) + "; difference: " + str(difference) + "; product: " + str(product))
-    
+    print(f"Sum: {one + two + three}; difference: {one - two - three}; \
+product: {one * two * three}")
+else:
+    print("Your number isn't three-digit")
