5b21cbc93cfd1e650151a27c2824b026991c3ebc
Maksym Davydenko <maksydenko@gmail.com>
2021-11-23 14:36:55 +0200

    Practice 6

--- practice_06.py
+++ practice_06.py
@@ -1,33 +1,34 @@
 # Task 1
 
-string_1 = input("Write something: ")
-print(f"Your string length: {len(string_1)} characters")
+str_1 = input("Write something: ")
+print(f"Your string length: {len(str_1)} characters")
 
 # Task 2
 
-string_2 = input("Write something: ")
+str_2 = input("Write something: ")
 
-if len(string_2) < 2:
+if len(str_2) < 2:
     print()
 else:
-    print(string_2[:2] + string_2[-2:])
+    print(str_2[:2] + str_2[-2:])
 
 # Task 3
 
-string_3 = input("Write something: ")
-print(string_3[0] + string_3[1:].replace(string_3[0], "$"))
+str_3 = input("Write something: ")
+print(str_3[0] + str_3[1:].replace(str_3[0], "$"))
 
 # Task 4
 
-string_4 = input("Write something: ")
+str_4 = input("Write something: ")
 
-if len(string_4) % 4:
+if len(str_4) % 4:
     print("Your string length isn't a multiple of 4")
 else:
-    for i in range(-1, -1 * len(string_4) - 1, -1):
-        print(string_4[i], end="")
+    for i in range(-1, -1 * len(str_4) - 1, -1):
+        print(str_4[i], end="")
+print()
 
 # Task 5
 
-words = input("Write comma separated some words: ")
-print(sorted(words.split(", ")))
+words = sorted(input("Write comma separated some words: ").split(", "))
+print(words)
