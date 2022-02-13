d6123cb0c74236743536617126b40efbd781c0e1
Maksym Davydenko <maksydenko@gmail.com>
2022-02-14 00:54:03 +0200

    Update main.py

--- practice_9/main.py
+++ practice_9/main.py
@@ -6,14 +6,11 @@ j = 0
 five = 0
 matrix = np.random.randint(0, 15, (10, 20))
 
-while i < len(matrix):
-    while j < len(matrix[i]):
+for i in range(10):
+    for j in range(20):
         if matrix[i][j] == 5:
             five += 1
-        j += 1
     if five >= 3:
         print(i)
-    i += 1
-    j = 0
     five = 0
 print(matrix)
