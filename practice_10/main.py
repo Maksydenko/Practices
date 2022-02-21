a33e102f542f2889721dabf727eff8b68af10acc
Maksym Davydenko <maksydenko@gmail.com>
2022-02-21 10:44:24 +0200

    Update main.py

--- practice_10/main.py
+++ practice_10/main.py
@@ -3,13 +3,13 @@ import random
 # Task 1
 
 def addRightDigit(d = random.randint(0, 10), k = 1000):
-    if type(d) and type(k) is int:
+    if isinstance(d, int) and isinstance(k, int):
         return k * 10 + d
 print(addRightDigit())
 
 # Task 2
 
 def numberPrefix(n = random.randint(1000, 100000), k = random.randint(1, 4)):
-    if type(n) and type(k) is int:
+    if isinstance(n, int) and isinstance(k, int):
         return str(n)[:k]
 print(numberPrefix())
