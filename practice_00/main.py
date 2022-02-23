796e921d65b49cebd05d7b8dd34371b00023ba13
Maksym Davydenko <maksydenko@gmail.com>
2022-02-23 17:05:08 +0200

    Create main.py

---
+++ practice_00/main.py
@@ -0,0 +1,41 @@
+import math
+
+# Task 1
+
+print("Hello world!")
+
+# Task 2
+
+print("How are you?\nThis is my first practice\nThat's wonderful!")
+
+# Task 3
+
+lenght = float(input("Write the length of the rectangle: "))
+width = float(input("Write the width of the rectangle: "))
+print("Square:", lenght * width)
+
+# Task 4
+
+first = float(input("Write the first number: "))
+second = float(input("Write the second number: "))
+print(f"Sum: {first + second}; product: {first * second}; \
+difference: {first - second}; quotient: {first / second}")
+
+# Task 5
+
+radius = float(input("Write the radius of the circle: "))
+print(f"Diameter: {radius * 2}; circumference: {radius * 2 * math.pi}; \
+area: {radius ** 2 * math.pi}")
+
+# Task 6
+
+number = int(input("Write the three-digit number: "))
+
+if 99 < number < 1000:
+    one = number // 100
+    two = number // 10 % 10
+    three = number % 10
+    print(f"Sum: {one + two + three}; difference: {one - two - three}; \
+product: {one * two * three}")
+else:
+    print("Your number isn't three-digit")
---
+++ practice_03/main.py
@@ -0,0 +1,42 @@
+# Task 1
+
+print([i for i in range(1, 10)])
+
+# Task 2
+
+print([i for i in range(9, 0, -1)])
+
+# Task 3
+
+print([i for i in range(5, 14, 2)])
+
+# Task 4
+
+sum = 0
+
+while True:
+    x = float(input("Write the number: "))
+    sum += x
+    if x == 0:
+        break
+print("Sum:", sum)
+
+# Task 5
+
+x = int(input("Write the number: "))
+y = 0
+
+while x > 0:
+    y *= 10
+    y += x % 10
+    x = x // 10
+print(y)
+
+# Task 6
+
+number = int(input("Write the number: "))
+factorial = 1
+
+for i in range(1, number + 1):
+    factorial *= number
+print("Factorial:", factorial)
