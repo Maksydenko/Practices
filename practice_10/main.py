8ab76a4ad79ef9c1e5e0053fbee242b6eeb09b1d
Maksym Davydenko <maksydenko@gmail.com>
2022-04-17 22:17:56 +0300

    Update main.py

--- practice_10/main.py
+++ practice_10/main.py
@@ -1,17 +1,66 @@
+import numpy as np
 import random
+from random import randint
 
 
 # Task 1
 
-def addRightDigit(d=random.randint(0, 10), k=1000):
-    if isinstance(d, int) and isinstance(k, int):
-        return k * 10 + d
-print(addRightDigit())
+def sortArray(array):
+    i = 0
+    flag = True
+    while flag:
+        flag = False
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                flag = True
+    return array
+number = int(input("Write the number of array elements: "))
+array = np.random.randint(-10, 10, number)
+print(sortArray(array), "\nMaximum is", sortArray(array)[-1], "in",
+      len(array) - 1, "\nMinimum is", sortArray(array)[0])
 
 
 # Task 2
 
-def numberPrefix(n=random.randint(1000, 100000), k=random.randint(1, 4)):
-    if isinstance(n, int) and isinstance(k, int):
-        return str(n)[:k]
-print(numberPrefix())
+def sortHalf(array):
+    full_len = len(array)
+    half_len = len(array) // 2
+    for i in range(half_len - 1):
+        min = i
+        for j in range(i + 1, half_len):
+            if array[j] < array[min]:
+                min = j
+        array[i], array[min] = array[min], array[i]
+    for i in range(half_len, full_len - 1):
+        min = i
+        for j in range(i + 1, full_len):
+            if array[j] > array[min]:
+                min = j
+        array[i], array[min] = array[min], array[i]
+    return array
+array = np.random.randint(-10, 10, 10)
+print(sortHalf(array))
+
+
+# Task 3
+
+def sortZeros(array):
+    for i in range(1, len(array)):
+        j = i - 1
+        key = array[i]
+        while j >= 0 and array[j] > key:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = key
+    zeros = 0
+    for i in range(len(array)):
+        if array[i] == 0:
+            zeros += 1
+            while array[i - 1] != 0 and i != 0:
+                print(array)
+                array[i], array[i - 1] = array[i - 1], array[i]
+                i -= 1
+    return array[zeros:]
+array = np.random.randint(-2, 5, 20)
+print(sortZeros(array))
