355b112af4b9516507c530461310671124ac6751
Maksydenko <maksydenko@gmail.com>
2022-05-09 12:49:28 +0300

    Update main.py

--- practice_11/main.py
+++ practice_11/main.py
@@ -4,36 +4,36 @@ import random
 
 # Task 1
 
-def myFac(fac):
+def my_fac(fac):
     if fac < 0:
         return "You wrote the number less than zero"
     if fac == 0:
         return 1
     else:
-        return fac * myFac(fac - 1)
+        return fac * my_fac(fac - 1)
 fac = int(input("Write the number: "))
-print(myFac(fac))
+print(my_fac(fac))
 
 
 # Task 2
 
-def digitalRoot(num):
+def digital_root(num):
     if 0 < num < 10:
         return num
     summ = 0
     if num >= 10:
         for i in str(num):
             summ += int(i)
-        return digitalRoot(summ)
+        return digital_root(summ)
     else:
         return "You wrote the number less than zero"
 num = int(input("White the number: "))
-print(digitalRoot(num))
+print(digital_root(num))
 
 
 # Task 3
 
-def maxIndex(array, rand):
+def max_index(array, rand):
     max, x, y = array[0, 0], 0, 0
     for i in range(rand):
         for j in range(rand):
@@ -42,5 +42,4 @@ def maxIndex(array, rand):
     return x, y
 rand = random.randint(1, 5)
 array = np.random.randint(-50, 50, (rand, rand))
-print(array)
-print("Index maximum number:", maxIndex(array, rand))
+print(array, "\nIndex maximum number:", max_index(array, rand))
