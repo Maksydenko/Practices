22bde13b35efc9b7fc439d767d423f40415c61f0
Maksydenko <maksydenko@gmail.com>
2022-05-14 15:32:28 +0300

    Update main.py

--- lab_09_01/main.py
+++ lab_09_01/main.py
@@ -1,12 +1,6 @@
-import timeit
 import numpy as np
 from random import randint
-
-# Setup code for performance evaluation indicators
-setup_code = """
-import numpy as np
-from random import randint
-"""
+from time import perf_counter
 
 
 # Bubble sort ascending
@@ -26,6 +20,7 @@ Answer (maximum 30): "))
             array = np.append(array, number)
 
     # Bubble sort in ascending order
+    start_time = perf_counter()
     i = 0
     flag = True
     while flag:
@@ -35,28 +30,12 @@ Answer (maximum 30): "))
                 array[j], array[j + 1] = array[j + 1], array[j]
                 flag = True
         i += 1
+    all_time = perf_counter() - start_time
     if answer == "YES":
-        return "Your array: ", custom_array, "\nSorted array: ", array
-    return "Sorted array: ", array
-# Performance evaluation indicators
-bubble1_code = """
-def bubble_ascending(array=np.random.randint(-1000, 1000, 10000)):
-    # Bubble sort in ascending order
-    i = 0
-    flag = True
-    while flag:
-        flag = False
-        for j in range(len(array) - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-                flag = True
-        i += 1
-"""
-time_bubble1 = timeit.timeit(setup=setup_code,
-                             stmt=bubble1_code,
-                             number=10)
+        return "Your array: ", custom_array, "Sorted array: ", array, "Sorting time", all_time
+    return "Sorted array: ", array, "Sorting time", all_time
 
-print(bubble_ascending(), "\nFunction execution time:", time_bubble1)
+print(bubble_ascending())
 
 
 # Bubble sort descending
@@ -76,6 +55,7 @@ Answer (maximum 30): "))
             array = np.append(array, number)
 
     # Bubble sort in descending order
+    start_time = perf_counter()
     i = 0
     flag = True
     while flag:
@@ -85,28 +65,12 @@ Answer (maximum 30): "))
                 array[j], array[j + 1] = array[j + 1], array[j]
                 flag = True
         i += 1
+    all_time = perf_counter() - start_time
     if answer == "YES":
-        return "Your array: ", custom_array, "\nSorted array: ", array
-    return "Sorted array: ", array
-# Performance evaluation indicators
-bubble2_code = """
-def bubble_ascending(array=np.random.randint(-1000, 1000, 10000)):
-    # Bubble sort in descending order
-    i = 0
-    flag = True
-    while flag:
-        flag = False
-        for j in range(len(array) - i - 1):
-            if array[j] < array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-                flag = True
-        i += 1
-"""
-time_bubble2 = timeit.timeit(setup=setup_code,
-                             stmt=bubble2_code,
-                             number=10)
+        return "Your array: ", custom_array, "Sorted array: ", array, "Sorting time", all_time
+    return "Sorted array: ", array, "Sorting time", all_time
 
-print(bubble_descending(), "\nFunction execution time:", time_bubble2)
+print(bubble_descending())
 
 
 # Selection sort ascending
@@ -126,31 +90,19 @@ Answer (maximum 30): "))
             array = np.append(array, number)
 
     # Selection sort in ascending order
+    start_time = perf_counter()
     for i in range(len(array) - 1):
         min = i
         for j in range(i + 1, len(array)):
             if array[j] < array[min]:
                 min = j
         array[i], array[min] = array[min], array[i]
+    all_time = perf_counter() - start_time
     if answer == "YES":
-        return "Your array: ", custom_array, "\nSorted array: ", array
-    return "Sorted array: ", array
-# Performance evaluation indicators
-selection1_code = """
-def selection_ascending(array=np.random.randint(-1000, 1000, 10000)):
-    # Selection sort in ascending order
-    for i in range(len(array) - 1):
-        min = i
-        for j in range(i + 1, len(array)):
-            if array[j] < array[min]:
-                min = j
-        array[i], array[min] = array[min], array[i]
-"""
-time_selection1 = timeit.timeit(setup=setup_code,
-                                stmt=selection1_code,
-                                number=10)
+        return "Your array: ", custom_array, "Sorted array: ", array, "Sorting time", all_time
+    return "Sorted array: ", array, "Sorting time", all_time
 
-print(selection_ascending(), "\nFunction execution time:", time_selection1)
+print(selection_ascending())
 
 
 # Selection sort descending
@@ -170,31 +122,19 @@ Answer (maximum 30): "))
             array = np.append(array, number)
 
     # Selection sort in descending order
+    start_time = perf_counter()
     for i in range(len(array) - 1):
         min = i
         for j in range(i + 1, len(array)):
             if array[j] > array[min]:
                 min = j
         array[i], array[min] = array[min], array[i]
+    all_time = perf_counter() - start_time
     if answer == "YES":
-        return "Your array: ", custom_array, "\nSorted array: ", array
-    return "Sorted array: ", array
-# Performance evaluation indicators
-selection2_code = """
-def selection_descending(array=np.random.randint(-1000, 1000, 10000)):
-    # Selection sort in ascending order
-    for i in range(len(array) - 1):
-        min = i
-        for j in range(i + 1, len(array)):
-            if array[j] > array[min]:
-                min = j
-        array[i], array[min] = array[min], array[i]
-"""
-time_selection2 = timeit.timeit(setup=setup_code,
-                                stmt=selection2_code,
-                                number=10)
+        return "Your array: ", custom_array, "Sorted array: ", array, "Sorting time", all_time
+    return "Sorted array: ", array, "Sorting time", all_time
 
-print(selection_descending(), "\nFunction execution time:", time_selection2)
+print(selection_descending())
 
 
 # Insertion sort ascending
@@ -214,6 +154,7 @@ Answer (maximum 30): "))
             array = np.append(array, number)
 
     # Insertion sort in ascending order
+    start_time = perf_counter()
     for i in range(1, len(array)):
         j = i - 1
         key = array[i]
@@ -221,26 +162,12 @@ Answer (maximum 30): "))
             array[j + 1] = array[j]
             j -= 1
         array[j + 1] = key
+    all_time = perf_counter() - start_time
     if answer == "YES":
-        return "Your array: ", custom_array, "\nSorted array: ", array
-    return "Sorted array: ", array
-# Performance evaluation indicators
-insertion1_code = """
-def insertion_ascending(array=np.random.randint(-1000, 1000, 10000)):
-    # Insertion sort in ascending order
-    for i in range(1, len(array)):
-        j = i - 1
-        key = array[i]
-        while j >= 0 and array[j] > key:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key
-"""
-time_insertion1 = timeit.timeit(setup=setup_code,
-                                stmt=insertion1_code,
-                                number=10)
+        return "Your array: ", custom_array, "Sorted array: ", array, "Sorting time", all_time
+    return "Sorted array: ", array, "Sorting time", all_time
 
-print(insertion_ascending(), "\nFunction execution time:", time_insertion1)
+print(insertion_ascending())
 
 
 # Insertion sort descending
@@ -260,6 +187,7 @@ Answer (maximum 30): "))
             array = np.append(array, number)
 
     # Insertion sort in descending order
+    start_time = perf_counter()
     for i in range(1, len(array)):
         j = i - 1
         key = array[i]
@@ -267,23 +195,9 @@ Answer (maximum 30): "))
             array[j + 1] = array[j]
             j -= 1
         array[j + 1] = key
+    all_time = perf_counter() - start_time
     if answer == "YES":
-        return "Your array: ", custom_array, "\nSorted array: ", array
-    return "Sorted array: ", array
-# Performance evaluation indicators
-insertion2_code = """
-def insertion_descending(array=np.random.randint(-1000, 1000, 10000)):
-    # Insertion sort in ascending order
-    for i in range(1, len(array)):
-        j = i - 1
-        key = array[i]
-        while j >= 0 and array[j] < key:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key
-"""
-time_insertion2 = timeit.timeit(setup=setup_code,
-                                stmt=insertion2_code,
-                                number=10)
+        return "Your array: ", custom_array, "Sorted array: ", array, "Sorting time", all_time
+    return "Sorted array: ", array, "Sorting time", all_time
 
-print(insertion_descending(), "\nFunction execution time:", time_insertion2)
+print(insertion_descending())
