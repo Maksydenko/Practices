0dfb789db0179bc17827ba95398b8d5a6e2558a9
Maksydenko <maksydenko@gmail.com>
2022-05-24 13:53:57 +0300

    Update main.py

--- practice_01/main.py
+++ practice_01/main.py
@@ -15,19 +15,26 @@ print(d)
 e = float(1e3)
 print(e)
 
-f, g, h = 7, -7, 0
+f = 7
+g = -7
+h = 0
 print(f, g, h)
 
-j, k = 2 + (f - 6) / g * 9, 5 ** h - (9 + f * -3)
+j = 2 + (f - 6) / g * 9
+k = 5 ** h - (9 + f * -3)
 print(j, k)
 
-l, m, n = 3.2, -11.15, 14.6
+l = 3.2
+m = -11.15
+n = 14.6
 print(l, m, n)
 
-o, p = 19 - m + 14 ** 2, 3 * n // (9 + l)
+o = 19 - m + 14 ** 2
+p = 3 * n // (9 + l)
 print(o, p)
 
-q, r = (f + l) / 2 - h, 9 + m - (g % 3) + n
+q = (f + l) / 2 - h
+r = 9 + m - (g % 3) + n
 print(q, r)
 
 s = float(f) / g
@@ -51,8 +58,11 @@ print(e1)
 card_number = int(input("Write your credit card number: "))
 
 if 999999999999999 < card_number < 10000000000000000:
-    print(f"The last 4 digits of a credit card: {card_number // 1000 % 10}\
-{card_number // 100 % 10}{card_number // 10 % 10}{card_number % 10}")
+    number_1 = str(card_number // 1000 % 10)
+    number_2 = str(card_number // 100 % 10)
+    number_3 = str(card_number // 10 % 10)
+    number_4 = str(card_number % 10)
+    print("The last 4 digits of a credit card:", number_1 + number_2 + number_3 + number_4)
 else:
     print("The wrong card number is specified")
     
@@ -62,7 +72,7 @@ if 99 < number < 1000:
     digit_1 = number // 100
     digit_2 = number // 10 % 10
     digit_3 = number % 10
-    print("The sum of the digits of a three-digit number:", 
-digit_1 + digit_2 + digit_3)
+    sum = digit_1 + digit_2 + digit_3
+    print("The sum of the digits of a three-digit number:", sum)
 else:
     print("Your number isn't three-digit")
