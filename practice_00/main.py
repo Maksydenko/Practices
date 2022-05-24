82f7a44c83dec009b6e17c17722c1338c8bd28d3
Maksydenko <maksydenko@gmail.com>
2022-05-24 13:53:34 +0300

    Update main.py

--- practice_00/main.py
+++ practice_00/main.py
@@ -12,20 +12,28 @@ print("How are you?\nThis is my first practice\nThat's wonderful!")
 
 lenght = float(input("Write the length of the rectangle: "))
 width = float(input("Write the width of the rectangle: "))
-print("Square:", lenght * width)
+square = lenght * width
+print("Square:", square)
 
 # Task 4
 
 first = float(input("Write the first number: "))
 second = float(input("Write the second number: "))
-print(f"Sum: {first + second}; product: {first * second}; \
-difference: {first - second}; quotient: {first / second}")
+sum = first + second
+product = first * second
+difference = first - second
+quotient = first / second
+print("Sum:", sum, "; product:", product,
+    "; difference:", difference, "; quotient:", quotient)
 
 # Task 5
 
 radius = float(input("Write the radius of the circle: "))
-print(f"Diameter: {radius * 2}; circumference: {radius * 2 * math.pi}; \
-area: {radius ** 2 * math.pi}")
+diameter = radius * 2
+circumference = diameter * math.pi
+area = radius ** 2 * math.pi
+print("Diameter:", diameter, "; circumference:", circumference,
+    "; area:", area)
 
 # Task 6
 
@@ -35,7 +43,9 @@ if 99 < number < 1000:
     one = number // 100
     two = number // 10 % 10
     three = number % 10
-    print(f"Sum: {one + two + three}; difference: {one - two - three}; \
-product: {one * two * three}")
+    sum = one + two + three
+    difference = one - two - three
+    product = one * two * three
+    print("Sum:", sum, "; difference:", difference, "; product:", product)
 else:
     print("Your number isn't three-digit")
