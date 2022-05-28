288a30b661a7e1dbda32e2f8a4e56d9e82789d59
Maksydenko <maksydenko@gmail.com>
2022-05-28 13:40:34 +0300

    Create main.py

---
+++ practice-10/main.py
@@ -0,0 +1,17 @@
+from random import randint
+
+
+# Task 1
+
+def addRightDigit(d=randint(0, 10), k=1000):
+    if isinstance(d, int) and isinstance(k, int):
+        return k * 10 + d
+print(addRightDigit())
+
+
+# Task 2
+
+def number_prefix(n=randint(1000, 100000), k=randint(1, 4)):
+    if isinstance(n, int) and isinstance(k, int):
+        return str(n)[:k]
+print(number_prefix())
